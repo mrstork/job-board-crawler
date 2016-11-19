f41df537fc03142574f26c2f744bcf883c2e7397
Mateusz Bocian <matbocian91@gmail.com>
2016-11-19 01:10:00 -0500

    Adding admin models

--- scrapers/admin.py
+++ scrapers/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Company, Position
 
-# Register your models here.
+def ignore(modeladmin, request, queryset):
+    for company in queryset:
+        company.ignore()
+ignore.short_description = "Ignore selected companies"
+
+class CompanyAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    actions = [ignore]
+    list_filter = (
+        ('ignored', admin.BooleanFieldListFilter),
+    )
+
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Position, PositionAdmin)
