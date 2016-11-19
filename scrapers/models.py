f4cc34b7371b54fad78b210ce9b3b49d0c2e7744
Mateusz Bocian <matbocian91@gmail.com>
2016-11-19 01:09:26 -0500

    Fixing ignore method and adding __str__ methods for each class

--- scrapers/models.py
+++ scrapers/models.py
@@ -5,10 +5,17 @@ class Company(models.Model):
     ignored = models.BooleanField(default=False)
 
     def ignore(self):
-        self.ignored = true
         Position.objects.filter(company=self).delete()
+        self.ignored = True
+        self.save()
+
+    def __str__(self):
+        return self.name
 
 class Position(models.Model):
     name = models.CharField(max_length=255)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
